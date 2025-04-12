c38caf13c4009e174b6a199bfb743583c0a26aa3
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-04-12 11:51:15 +0300

    Implemented comment service

---
+++ client/src/api/comments.ts
@@ -0,0 +1,35 @@
+import { Comment } from "../types/comment";
+import { Video } from "../types/video";
+import { del, get, post, put } from "./requester";
+
+const endpoint="comments";
+
+export async function getCommentById(commentId:string){
+    const comment=await get(`${endpoint}/${commentId}`);
+    return comment as Comment;
+}
+
+export async function createComment(videoId:string,data:object){
+    const comment=await post(`${endpoint}/in/${videoId}`,data);
+    return comment as Comment;
+}
+
+export async function deleteComment(videoId:string,commentId:string){
+    const updatedVideo=await del(`${endpoint}/${commentId}/in/${videoId}`);
+    return updatedVideo as Video;
+}
+
+export async function editComment(commentId:string,data:object){
+    const updatedComment=await put(`${endpoint}/${commentId}`,data);
+    return updatedComment as Comment;
+}
+
+export async function likeComment(commentId:string){
+    const comment=await get(`${endpoint}/like/${commentId}`);
+    return comment as Comment;
+}
+
+export async function unlikeComment(commentId:string){
+    const comment=await get(`${endpoint}/unlike/${commentId}`);
+    return comment as Comment;
+}
