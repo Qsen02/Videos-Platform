dbdc38bfab56025b84338175a5ad4d0a6eb92fcf
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-04-12 15:01:43 +0300

    Search form implemented

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,3 +1,22 @@
+import { useUserThemeContext } from "../../contexts/UserAndTheme";
+import styles from "./HomeStyles.module.css";
+
 export default function Home() {
-	return <h1>Home!</h1>;
+	const {theme}=useUserThemeContext();
+	return (
+		<>
+			<form className={styles.form}>
+				<p>
+					<input type="text" name="query" placeholder="Search..." className={theme=="dark"?"darkTheme-dark":"whiteTheme-light"}/>
+					<button className={theme=="dark"?"darkTheme-dark":"whiteTheme-light"}>
+						<i className="fa-solid fa-magnifying-glass"></i>
+					</button>
+				</p>
+				<select name="criteria" className={theme=="dark"?"darkTheme-dark":"whiteTheme-light"}>
+					<option value="videos">Videos</option>
+					<option value="users">Users</option>
+				</select>
+			</form>
+		</>
+	);
 }
