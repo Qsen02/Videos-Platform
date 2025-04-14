48654ea8ee1bff513cab4ec0216eb693c004b16c
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-04-14 11:17:45 +0300

    Implemented register functionallity

--- client/src/components/register/Register.tsx
+++ client/src/components/register/Register.tsx
@@ -1,43 +1,139 @@
-import { Form, Formik } from "formik";
+import { Form, Formik, FormikHelpers } from "formik";
 import CustomInput from "../../commons/customInput";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useRegister } from "../../hooks/useUsers";
 import { useUserThemeContext } from "../../contexts/UserAndTheme";
+import { registerShema } from "../../schemas/validationShema";
+import { useState } from "react";
 
-export default function Register(){
-    const {setUser,theme}=useUserThemeContext();
-    const register=useRegister();
+export default function Register() {
+	const { setUser, theme } = useUserThemeContext();
+	const register = useRegister();
+	const navigate = useNavigate();
+	const [isErr, setIsErr] = useState(false);
+	const [errMessage, setErrMessage] = useState("");
 
-    const initialValues={
-        username:"",
-        email:"",
-        profileImage:"",
-        password:"",
-        repass:""
-    }
+	const initialValues = {
+		username: "",
+		email: "",
+		profileImage: "",
+		password: "",
+		repass: "",
+	};
 
-    async function onRegister(){
+	interface FormValues {
+		username: string;
+		email: string;
+		profileImage: string;
+		password: string;
+		repass: string;
+	}
 
-    }
+	async function onRegister(
+		values: FormValues,
+		actions: FormikHelpers<FormValues>
+	) {
+		try {
+			const username = values.username;
+			const email = values.email;
+			const profileImage = values.profileImage;
+			const password = values.password;
+			const repass = values.repass;
+			const user = await register({
+				username,
+				email,
+				profileImage,
+				password,
+				repass,
+			});
+			if (setUser) {
+				setUser(user);
+			}
+            actions.resetForm();
+            navigate("/");
+		} catch (err) {
+			setIsErr(true);
+			if (err instanceof Error) {
+				setErrMessage(err.message);
+			} else {
+				setErrMessage("Error occurd!");
+			}
+		}
+	}
 
-    return (
-        <Formik initialValues={initialValues} onSubmit={onRegister}>
-            {
-                (props)=>(
-                    <Form className={`form ${theme == "dark" ? "darkTheme-dark" : "whiteTheme-light"}`}>
-                        <h2>Here you can create your account.</h2>
-                        <p className="input">
-                            <CustomInput label="Username" type="text" name="username" className={theme == "dark" ? "darkTheme-light" : "whiteTheme-darkWhite"}/>
-                            <CustomInput label="Email" type="text" name="email" className={theme == "dark" ? "darkTheme-light" : "whiteTheme-darkWhite"}/>
-                            <CustomInput label="Profile image" type="text" name="profileImage" className={theme == "dark" ? "darkTheme-light" : "whiteTheme-darkWhite"}/>
-                            <CustomInput label="Password" type="text" name="password" className={theme == "dark" ? "darkTheme-light" : "whiteTheme-darkWhite"}/>
-                            <CustomInput label="Repeat password" type="text" name="repass" className={theme == "dark" ? "darkTheme-light" : "whiteTheme-darkWhite"}/>
-                        </p>
-                        <button type="submit">Submit</button>
-                        <p>Already have account? You can <Link to="/login">login</Link> here.</p>
-                    </Form>
-                )
-            }
-        </Formik>
-    )
+	return (
+		<Formik
+			initialValues={initialValues}
+			onSubmit={onRegister}
+			validationSchema={registerShema}
+		>
+			{(props) => (
+				<Form
+					className={`form ${
+						theme == "dark" ? "darkTheme-dark" : "whiteTheme-light"
+					}`}
+				>
+					<h2>Here you can create your account.</h2>
+					{isErr ? <p className="error">{errMessage}</p> : ""}
+					<p className="input">
+						<CustomInput
+							label="Username"
+							type="text"
+							name="username"
+							className={
+								theme == "dark"
+									? "darkTheme-light"
+									: "whiteTheme-darkWhite"
+							}
+						/>
+						<CustomInput
+							label="Email"
+							type="text"
+							name="email"
+							className={
+								theme == "dark"
+									? "darkTheme-light"
+									: "whiteTheme-darkWhite"
+							}
+						/>
+						<CustomInput
+							label="Profile image"
+							type="text"
+							name="profileImage"
+							className={
+								theme == "dark"
+									? "darkTheme-light"
+									: "whiteTheme-darkWhite"
+							}
+						/>
+						<CustomInput
+							label="Password"
+							type="text"
+							name="password"
+							className={
+								theme == "dark"
+									? "darkTheme-light"
+									: "whiteTheme-darkWhite"
+							}
+						/>
+						<CustomInput
+							label="Repeat password"
+							type="text"
+							name="repass"
+							className={
+								theme == "dark"
+									? "darkTheme-light"
+									: "whiteTheme-darkWhite"
+							}
+						/>
+					</p>
+					<button type="submit">Submit</button>
+					<p>
+						Already have account? You can{" "}
+						<Link to="/login">login</Link> here.
+					</p>
+				</Form>
+			)}
+		</Formik>
+	);
 }
---
+++ client/src/schemas/validationShema.ts
@@ -0,0 +1,23 @@
+import * as yup from "yup";
+
+export const registerShema = yup.object().shape({
+	username: yup
+		.string()
+		.min(3, "Username must be at least 3 symbols long!")
+		.required("Username is required!"),
+	email: yup.string().email("Email must be valid email!").required("Email is required!"),
+	profileImage: yup
+		.string()
+		.matches(/^https?:\/\//, "Profile image must be valid URL"),
+	password: yup
+		.string()
+		.matches(
+			/^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/,
+			"Password must be at least 6 symbols ant must contain digits, letters and at least one capital letter and special symbol!"
+		)
+		.required("Password is required!"),
+	repass: yup
+		.string()
+		.oneOf([yup.ref("password")], "Password must match!")
+		.required("Repeat password is required!"),
+});
