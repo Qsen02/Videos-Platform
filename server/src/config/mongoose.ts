8bcbbf452305e971dffd24c8d6db736fe933b361
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-05-08 10:49:46 +0300

    update

--- server/src/config/mongoose.ts
+++ server/src/config/mongoose.ts
@@ -5,10 +5,10 @@ import { Comments } from "../models/comments";
 import dotenv from "dotenv";
 dotenv.config();
 
-const localDB="mongodb://127.0.0.1:27017/Videos-Platfrom";
+// const localDB="mongodb://127.0.0.1:27017/Videos-Platfrom";
 
 export async function runDB(){
-    await mongoose.connect(localDB);
+    await mongoose.connect(process.env.PRODDB!);
     await Users;
     await Videos;
     await Comments;
